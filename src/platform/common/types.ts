6d6c9cb762b996dfa7a77f4ab08990daea9a9b90
Peng Lyu <penn.lv@gmail.com>
2024-10-09 14:08:54 -0700

    Update formatting

--- src/platform/common/types.ts
+++ src/platform/common/types.ts
@@ -2,10 +2,10 @@
 // Licensed under the MIT License.
 
 export type ReadWrite<T> = {
-    -readonly [P in keyof T]: T[P];
+	-readonly [P in keyof T]: T[P];
 };
 
 export type ClassType<T> = {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    new (...args: any[]): T;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	new (...args: any[]): T;
 };
