5f8cb1736b8c28c84f0f78ed847cc7fe7e590a9c
Don Jayamanne <don.jayamanne@outlook.com>
2024-10-11 10:48:10 +1100

    Commit the generated file to the repo

---
+++ build/archive.ts
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as path from 'path';
+import * as fs from 'fs';
+import archiver from 'archiver';
+
+function main() {
+    // create a file to stream archive data to.
+    const outputFile = path.join(__dirname, '..', 'release', 'pyodide.zip');
+    if (fs.existsSync(outputFile)) {
+        fs.rmSync(outputFile);
+    }
+    if (!fs.existsSync(path.dirname(outputFile))) {
+        fs.mkdirSync(path.dirname(outputFile), { recursive: true });
+    }
+    const output = fs.createWriteStream(outputFile);
+    const archive = archiver('zip', {});
+
+    // pipe archive data to the file
+    archive.pipe(output);
+    // append files from a sub-directory, putting its contents at the root of archive
+    archive.directory(path.join(__dirname, '..', 'pyodide/'), false);
+    archive.finalize();
+    console.log('Pyodide (release) archive created in release/pyodide.zip');
+}
+
+main();
