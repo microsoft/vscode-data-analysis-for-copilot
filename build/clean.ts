8fb69be8080a6aa1eeff9cac37cb9f13fab51ceb
Don Jayamanne <don.jayamanne@outlook.com>
2024-10-10 23:31:54 +1100

    Delete pyodide dir as well when building

--- build/clean.ts
+++ build/clean.ts
@@ -7,6 +7,9 @@ import * as path from 'path';
 if (fs.existsSync(path.join(__dirname, '..', 'out'))) {
 	fs.rmSync(path.join(__dirname, '..', 'out'), { recursive: true });
 }
+if (fs.existsSync(path.join(__dirname, '..', 'pyodide'))) {
+	fs.rmSync(path.join(__dirname, '..', 'pyodide'), { recursive: true });
+}
 if (fs.existsSync(path.join(__dirname, '..', 'temp'))) {
 	fs.rmSync(path.join(__dirname, '..', 'temp'), { recursive: true });
 }
