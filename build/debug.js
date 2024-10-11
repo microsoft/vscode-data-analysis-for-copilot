6d4219f7402ffbddcf38cff51b4069f884083ea9
Don Jayamanne <Don.Jayamanne@microsoft.com>
2024-10-11 13:50:32 +1100

    Added ability to debug worker

--- build/debug.js
+++ build/debug.js
@@ -1,21 +1,17 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const {Kernel} = require( '../pyodide/node/index');
+const { Kernel } = require('../pyodide/node/index');
 
-const pyodideDir = '<pyodide dir>';
+const pyodideDir = '<pyodide>';
 
-async function main(){
-    const kernel = new Kernel(pyodideDir,
-        `${pyodideDir}/node/comlink.worker.js`,
-        __dirname
-    );
+async function main() {
+    const kernel = new Kernel(pyodideDir, `${__dirname}/../pyodide/node/comlink.worker.js`, __dirname);
     try {
-        const outputs = await  kernel.execute('print(1243)')
+        const outputs = await kernel.execute('print(1243)');
         console.log(outputs);
-    } catch (ex){
+    } catch (ex) {
         // eslint-disable-next-line no-debugger
         debugger;
         console.log(ex);
